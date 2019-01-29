ef2075a9321cf70ac61f6951c4fe6ad026311f48
Arthur Alcaraz <arthur.alcaraz@gmail.com>
2019-01-29 12:44:16 +0100

    added constraints

--- xcsp_converter.py
+++ xcsp_converter.py
@@ -65,6 +65,37 @@ for v in variables_data:
     ET.SubElement(variables, "variable", {"name":"x{}".format(v[0]),
         "domain":"domain{}".format(v[1]),
         "agent":"F{}".format(v[0])})
-    
-print(prettify(instance))
 
+predicates_data = [
+        {
+                "name":"SUP",
+                "parameters": "int X1 int X2 int K",
+                "function": "gt(abs(sub(X1, X2)), K)"
+        },
+        {
+                "name":"EQ",
+                "parameters": "int X1 int X2 int K",
+                "function": "eq(abs(sub(X1, X2)), K)"
+        }
+]
+
+predicates = ET.SubElement(instance, "predicates", {"nbPredicates":str(len(predicates_data))})
+for p in predicates_data:
+        p_tmp = ET.SubElement(predicates, "predicate", {"name":p["name"]})
+        param_tmp = ET.SubElement(p_tmp, "parameters").text = p["parameters"]
+        exp_tmp = ET.SubElement(p_tmp, "expression")
+        funct_tmp = ET.SubElement(exp_tmp, "functional").text = p["function"]
+
+constraints = ET.SubElement(instance, "constraints", {"nbConstraints":str(len(constraints_data))})
+
+for c in constraints_data:
+        ctr_tmp = ET.SubElement(constraints, "constraint",{
+                "name":"x{}_x{}".format(c[0],c[1]),
+                "scope":"x{} x{}".format(c[0],c[1]),
+                "arity":"2",
+                "reference":"{}".format("EQ" if c[3] == "=" else "SUP")
+        })
+        param_tmp = ET.SubElement(ctr_tmp,"parameter").text = "x{} x{} {}".format(c[0], c[1], c[4])
+
+
+print(prettify(instance))
